3ecc9f2584ea3580aa4bbe14cf0dad2dde2029ee
Skumar101 <89221642+Skumar101@users.noreply.github.com>
2022-02-08 15:10:30 -0600

    wordle

---
+++ Intro Code/WordList2022.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;//required for AL
+import java.util.Scanner;
+import java.io.*;//required for File i/o
+
+public class WordList2022
+{
+  public static void main (String [] args) throws IOException // required for File IO
+   {
+	   ArrayList myList = new ArrayList();;
+	   Scanner scanFile = new Scanner(new File("filename.txt"));
+}
+}
