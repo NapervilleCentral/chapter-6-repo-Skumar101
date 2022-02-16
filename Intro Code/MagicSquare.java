ab17945821659979d8d4719ed69969667d38279e
Skumar101 <89221642+Skumar101@users.noreply.github.com>
2022-02-16 15:09:17 -0600

    magic square

--- Intro Code/MagicSquare.java
+++ Intro Code/MagicSquare.java
@@ -1,3 +1,4 @@
+;
 /**
  * Write a description of class MagicSquare here.
  *
@@ -18,6 +19,18 @@ public class MagicSquare
     }
     public boolean isMagic()
     {
+        return checkrows()&&checkdiagonals()&&checkcoloumns();
+    }
+    public int getMagicNum()
+    {
+        return magicNum;
+    }
+    public void setMagicSquare(int [] [] array)
+    {
+        this.array=array;
+    }
+    public boolean checkrows()
+    {
         int truecounter = 0;
         for(int i = 1; i<array.length; i++)
         {
@@ -28,11 +41,49 @@ public class MagicSquare
             }
             if(num==magicNum)
                 truecounter++;
+            else
+                break;
         }
         if(truecounter == array.length-1)
             return true;
         else
             return false;
     }
-    //public int
+    public boolean checkdiagonals()
+    {
+        int all = 0;
+        for(int i = 0; i<array[0].length; i++)
+        {
+            all+=array[i][i];
+        }
+        int all2 = 0;
+        for(int i = array[0].length-1; i>=0; i--)
+        {
+            all2+=array[i][array[0].length-1-i];
+        }
+        if(all==magicNum && all2==magicNum)
+            return true;
+        else
+            return false;
+    }
+    public boolean checkcoloumns()
+    {
+        int truecounter = 0;
+        for(int i = 1; i<array.length; i++)
+        {
+            int num = 0;
+            for(int j = 0; j<array.length; j++)
+            {
+                num+=array[j][i];
+            }
+            if(num==magicNum)
+                truecounter++;
+            else
+                break;
+        }
+        if(truecounter == array.length-1)
+            return true;
+        else
+            return false;
+    }
 }
