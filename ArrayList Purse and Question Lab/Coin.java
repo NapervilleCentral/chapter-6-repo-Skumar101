d5b843440afa5860e42d55fb517cf40716f1df05
Skumar101 <89221642+Skumar101@users.noreply.github.com>
2022-02-23 15:09:56 -0600

    purse and coin and stuff

--- ArrayList Purse and Question Lab/Coin.java
+++ ArrayList Purse and Question Lab/Coin.java
@@ -18,9 +18,20 @@ public class Coin
    /**-----------------------------------------------------------------
    *  Sets up the coin by flipping it initially.
    */
-   public Coin ()
+   public Coin (char Type)
    {
-      flip();
+       if (Type == 'p')
+        amount = 0.01;
+       else if(Type == 'n')
+        amount = 0.05;
+       else if(Type == 'd')
+        amount = 0.1;
+       else if(Type == 'q')
+        amount = 0.25;
+       else
+        amount = 0;
+       type = Type;
+       flip();
    }
    
    /**-----------------------------------------------------------------
@@ -34,9 +45,9 @@ public class Coin
    /**------------------------------------------------------------------
    * does stuff
    */
-   public int getValue()
+   public double getValue()
    {
-       return 0;
+       return amount;
     }
    
    /**-------------------------------------------------------------------
@@ -44,7 +55,7 @@ public class Coin
     */ 
    public char getType()
    {
-       if(
+       return type;
     }
    /**-----------------------------------------------------------------
    *  Returns true if the current face of the coin is heads.
@@ -56,17 +67,22 @@ public class Coin
    }
 
    /**-----------------------------------------------------------------
-   *  Returns the current face of the coin as a string.
+   *  Returns coin type and value.
         @return String
    */
    public String toString()
    {
-      String faceName;
-      if (face == HEADS)
-         faceName = "Heads";
-      else
-         faceName = "Tails";
-
-      return faceName;
+      String Type;
+      if (type == 'p')
+        Type = "Penny";
+       else if(type == 'n')
+        Type = "Nickel";
+       else if(type == 'd')
+        Type = "Dime";
+       else if(type == 'q')
+        Type = "Quarter";
+       else
+        Type = "N/A";
+      return Type + " "+ amount;
    }
 }
--- ArrayList Purse and Question Lab/Purse.java
+++ ArrayList Purse and Question Lab/Purse.java
@@ -3,32 +3,47 @@ import java.util.ArrayList;
 
 public class Purse
 {
-	//private ArrayList
-
-
-	public Purse (){ };
-
-	public Purse (Coin acoin)
-	{
-		change.add(acoin);
-	}
-  /** precondition: Coin will be initialized (value and name) correctly
-  *               each variable will have a value
-   	  postcondition: Puts the Coin into the array list change
-  */
-	public void add (Coin acoin)
-	{
-
-
-
-	}
+    //private ArrayList
+    private ArrayList change;
+        public Purse (int p, int n, int d, int q)
+        {
+            while(p>0)
+                {change.add(new Coin('p'));
+                 p--;
+                }
+            while(n>0)
+                {change.add(new Coin('n'));
+                 n--;
+                }
+            while(d>0)
+                {change.add(new Coin('d'));
+                 d--;
+                }
+            while(q>0)
+                {change.add(new Coin('q'));
+                 q--;
+                }
+        }
+        
+        public Purse (Coin acoin)
+        {
+            change.add(acoin);
+        }
+        /** precondition: Coin will be initialized (value and name) correctly
+        *               each variable will have a value
+          postcondition: Puts the Coin into the array list change
+        */
+    public void add (Coin acoin)
+    {
+        change.add(acoin);
+    }
 
   /** Returns the total value of the coins in the array
   *                 list change
-  		@return ???
+        @return ???
   */
-	public double getTotal()
-	{
+    public double getTotal()
+    {
 
 
 		return 0;
